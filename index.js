d8f0c23709f9af02a431fe4a4f0983892d722f73
Kylee Sawyer <k.sawyer@ufl.edu>
2024-04-22 09:16:34 -0400

    added poem stanzas fuction

--- index.js
+++ index.js
@@ -13,16 +13,18 @@ const makeH2 = makeTag('h2')
 const makeEm = makeTag('em')
 const makeH3 = makeTag('h3')
 const makeP = makeTag('p')
-const makeBr = makeTag('br')
 
 const makeAuthor = pipe(makeEm, makeH3)
-const makeStanzas = pipe(makeP, makeBr)
 
 // complete this function
+// for (let i = 0; i < data[0].lines.length; i++) {
+//   var stanza = ""
+// }
+
 const makePoemHTML = data => 
 makeH2(data[0].title) + 
-makeAuthor("by " + data[0].author) + 
-console.log(data[0].lines)
+makeAuthor("by " + data[0].author) +
+data[0].lines.join(`<br/>`).split("<br/><br/>").map(makeP).join("")
 
 
 // data[0].lines.filter(lines => lines !== "")
@@ -31,6 +33,4 @@ console.log(data[0].lines)
 getPoemBtn.onclick = async function() {
   // renders the HTML string returned by makePoemHTML to #poem
   poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
-  console.log(poemEl)
-
 }
